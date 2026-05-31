3b4e60bd6c994222da7ae0bcb3f0586eb55c1fdf
PratakshTomar <pratyakshtomar2000@gmail.com>
2026-05-31 15:52:30 +0530

    easy code

--- youtube_manager_db.py
+++ youtube_manager_db.py
@@ -10,25 +10,10 @@ CREATE TABLE IF NOT EXISTS videos (
                id INTEGER PRIMARY KEY,
                name TEXT NOT NULL,
                time TEXT NOT NULL)''')
-def load_data():
-    cursor.execute("SELECT name, time FROM videos")
-    rows = cursor.fetchall()
-    videos = [{"name": row[0], "time": row[1]} for row in rows]
-    return videos       
-def save_data_helper(videos):
-    cursor.execute("DELETE FROM videos")    
-    for video in videos:
-        cursor.execute("INSERT INTO videos (name, time) VALUES (?, ?)", (video["name"], video["time"]))
-    conn.commit()
 def list_videos():
-    cursor.execute("SELECT name, time FROM videos")
-    rows = cursor.fetchall()
-    print("\n")
-    print('*'*70)
-    for index, row in enumerate(rows, start=1):
-        print(f"{index}.{row[0]},Duration: {row[1]}")
-    print("\n")
-    print('*'*70)
+    cursor.execute("SELECT * FROM videos")
+    for rows in cursor.fetchall():
+     print(rows)
 def add_videos():
     name=input("enter video name: ")
     time=input("enter video time: ")
@@ -36,28 +21,16 @@ def add_videos():
     conn.commit()   
 def update_video():
     list_videos()
-    index=int(input("enter video index to update: "))
-    cursor.execute("SELECT id FROM videos")
-    rows = cursor.fetchall()
-    if 1 <= index <= len(rows):
-        name=input("enter new video name: ")
-        time=input("enter new video time: ")
-        video_id = rows[index-1][0]
-        cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (name, time, video_id))
-        conn.commit()
-    else:
-        print("Invalid index. Please try again.")   
+    video_id=int(input("enter video ID to update: "))
+    name=input("enter new video name: ")
+    time=input("enter new video time: ")
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (name, time, video_id))
+    conn.commit()
 def remove_video():
     list_videos()
-    index=int(input("enter video index to remove: "))
-    cursor.execute("SELECT id FROM videos")
-    rows = cursor.fetchall()
-    if 1 <= index <= len(rows):
-        video_id = rows[index-1][0]
-        cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
-        conn.commit()
-    else:
-        print("Invalid index. Please try again.")   
+    video_id=int(input("enter video ID to remove: "))
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
+    conn.commit()  
 def main():
     while True:
         print("Youtube Manager with db || choose an option:")
